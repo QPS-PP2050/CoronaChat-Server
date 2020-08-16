66ea48f042ffe0a2e23f4151b6a928d1b550dd11
Louis Manabat <manabat.louis@gmail.com>
2020-08-17 01:47:19 +1000

    [feat] Finished registration/login code
    This is the bare code for creating accounts and login in with them.
    - To use the request.rest file, you will need to download the REST Client VS Code extension (ext ID: humao.rest-client).
    - Code will need to be modified to be able to work with an SQL database.
    - Test cases will be pushed when made and completed

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,24 +5,66 @@ const app = express();
 
 app.use(express.json());
 
-const users = [{}];
+const users: { email: any; password: any; }[] = [];
 
 app.get('/users', (req, res) => {
-    res.json(users)
+    res.json(users);
 });
 
+// Register POST request
 app.post('/users', async (req, res) => {
-    try {
-        var combine = req.body.email + req.body.password;
-        const hashedPassword = await bcrypt.hash(combine, 10);
-        const user = { name: req.body.email, password: hashedPassword };
-        users.push(user);
-        res.status(201).send();
-    } catch {
-        res.status(500).send();
+    var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var compare = req.body.email.match(emailRegex);
+    if (!compare) {
+        return res.status(400).send('Email/username is using invalid characters');
+    } else {
+        const user = users.find(user => user.email === req.body.email);
+        if (user != null) {
+            return res.status(400).send('Account under that email/username already exists');
+        } else {
+            try {
+                // var concat = req.body.email + req.body.password;
+                const hashedPassword = await bcrypt.hash(req.body.password, 10);
+                const user = { email: req.body.email, password: hashedPassword };
+                users.push(user);
+                res.status(201).send('Account created');
+            } catch {
+                res.status(500).send();
+            }
+        }
     }
-    
-    
 });
+
+// Login POST request
+app.post('/users/login', async (req, res) => {
+    const user = users.find(user => user.email === req.body.email);
+    if (user == null) {
+        return res.status(400).send('Account under this email/username does not exist');
+    } else {
+        try {
+            // var concat = req.body.email + req.body.password;
+            if (!await bcrypt.compare(req.body.password, user.password)) {
+                res.send('Login failed');
+            } else {
+                res.send('Success');
+            }
+        } catch {
+            res.status(500).send();
+        }
+    }
+});
+
+
+// Trying to make POST functions a bit cleaner by adding a modular function...
+// Didn't work but will keep it here if can figure out how to fix it
+// function checkEmail(email: any) {
+//     app.post('/users', async (req, res) => {
+//         const user = users.find(user => user.email === email);
+//         if (user != null)
+//             return false;
+//         else
+//             return true;
+//     });
+// };
 
 app.listen(3000);
