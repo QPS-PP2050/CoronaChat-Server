5394bf667f07998581d468eb5f0e9c8226f9d154
Louis Manabat <manabat.louis@gmail.com>
2020-08-31 18:36:08 +1000

    [Task] Worked on TypeORM insert script

--- src/index.ts
+++ src/index.ts
@@ -9,20 +9,48 @@ const http = new HttpServer();
 new ChatServer(http.server);
 new Api(http.app);
 
-// Example Code for how to work with TypeORM
 import { connect } from './orm/dbConfig';
 import {User} from "./orm/entities/User";
+import { getConnection } from "typeorm";
 
 connect().then(async connection => {
 
-    console.log("Inserting a new user into the database...");
+    await getConnection()
+        .createQueryBuilder()
+        .delete()
+        .from(User)
+        .execute();
+
+    console.log("Inserting a new user into the database....");
     const user = new User();
-    user.username = "test";
-    user.password = "test";
-    user.email = "test";
-    user.salt = "test";
+    user.id = 1;
+    user.username = "louis-manabat";
+    user.password = "testing123";
+    user.email = "louis@hotmail.com";
     await connection.manager.save(user);
     console.log("Saved a new user with id: " + user.id);
+
+
+    console.log("Loading latest user from the database...");
+    const latestUser = await connection
+        .createQueryBuilder()
+        .select("user")
+        .from(User, "user")
+        .orderBy("user.id", "DESC")
+        .limit(1)
+        .getOne();
+
+    console.log("Newest user: ", latestUser);
+    
+    console.log("Adding new user: ");
+    const user2 = new User();
+    user2.id = 1;
+    user2.username = "louis-manabata";
+    user2.password = "testing1233";
+    user2.email = "louis@hotmail.com";
+    await connection.manager.save(user2);
+    console.log("Saved a new user with id: " + user2.id);
+    
 
     console.log("Loading users from the database...");
     const users = await connection.manager.find(User);
