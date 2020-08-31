e0b9d4f7b6298b55d13e2ec7cabfaacea657e81f
Louis Manabat <manabat.louis@gmail.com>
2020-08-31 19:16:19 +1000

    [Task] Adjust some code for adding

--- src/index.ts
+++ src/index.ts
@@ -9,17 +9,25 @@ const http = new HttpServer();
 new ChatServer(http.server);
 new Api(http.app);
 
+// Example Code for how to work with TypeORM
 import { connect } from './orm/dbConfig';
 import {User} from "./orm/entities/User";
+import { getConnection } from "typeorm";
 
 connect().then(async connection => {
+
+    await getConnection()
+        .createQueryBuilder()
+        .delete()
+        .from(User)
+        .execute();
 
     console.log("Inserting a new user into the database....");
     const user = new User();
     user.id = 1;
-    user.username = "tony-baker";
+    user.username = "jayden-padayachee";
     user.password = "testing123";
-    user.email = "tony@hotmail.com";
+    user.email = "jayden@hotmail.com";
     await connection.manager.save(user);
     console.log("Saved a new user with id: " + user.id);
 
@@ -31,27 +39,23 @@ connect().then(async connection => {
         .from(User, "user")
         .orderBy("user.id", "DESC")
         .limit(1)
-        .getOne();
+        .execute();
 
     console.log("Newest user: ", latestUser);
     
     console.log("Adding new user: ");
     const user2 = new User();
     user2.id = 1;
-    user2.username = "johnson-chen";
+    user2.username = "allan-baker";
     user2.password = "testing1233";
-    user2.email = "ljohnson@hotmail.com";
+    user2.email = "allan@hotmail.com";
     await connection.manager.save(user2);
     console.log("Saved a new user with id: " + user2.id);
     
 
     console.log("Loading users from the database...");
-    const allUsers = await connection
-        .createQueryBuilder()
-        .select("user")
-        .from(User, "user")
-        .orderBy("user.id", "DESC")
-        console.log("All users from newest to oldest: ", allUsers);
+    const users = await connection.manager.find(User);
+    console.log("Loaded users: ", users);
 
     console.log("Here you can setup and run express/koa/any other framework.");
 
