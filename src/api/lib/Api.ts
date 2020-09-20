ac916e2512710c9e25663e9ee22a7456df13c183
Louis Manabat <manabat.louis@gmail.com>
2020-09-20 23:10:06 +1000

    [Task] Editing code for email changing

--- src/api/lib/Api.ts
+++ src/api/lib/Api.ts
@@ -7,7 +7,7 @@ import { User } from "./../../orm/entities/User";
 var bodyParser = require('body-parser');
 
 /*
-The following code works on changing the account's username.
+The following code works on changing the account's email.
 
 To use and test the database:
 1. You may use Postman or use the Rest Client VSCode extenstion (ext ID: humao.rest-client).
@@ -17,9 +17,9 @@ To use and test the database:
     file to see what you need to do).
 4. If the Rest Client is installed, above the POST request link, there is a button that
     says "Send Request".
-5. To test username change, send a request under the change username request comment. You should
-    be notified that the username for that account has been changed and pressing it again shoul tell
-    that said username already exists.
+5. To test email change, send a request under the change email request comment. You should
+    be notified that the email for that account has been changed and pressing it again shoul tell
+    that said email already exists.
 6. To check, send a request under the show users request comment to see the list of users 
     and their details.
 */
@@ -50,42 +50,42 @@ export class Api {
             }
         });
 
-        // The following method is to change a user's username
-        this.app.post('/users/changeusername', async (req, res) => {
-            /*
-            The username regex variable will be compared with the username
-            the user provides. The username will be considered valid
-            based on certain conditions:
-            - If there are no illegal characters 
-            - If the beginning character is a letter
-            - Only contains lowercase letters and numbers between 0 to 9
-            - Is between 8 to 16 characters
+        // The following method is to change a user's email
+        this.app.post('/users/changeemail', async (req, res) => {
+            /* 
+            The email regex variable will be compared with the
+                email the user provides. The email will be considered valid
+                based on certain conditions:
+            - If there are no illegal characters (only dash and underscore allowed)
+            - If the beginning character is alphanumeric
+            - An '@' is present and does not have a dot before or after it
+            - No consecutive dots 
             */
-            var usernameRegex = /^\D[a-z0-9]{8,16}$/;
-            // Var below will compare the user input with regex above to see if it is a valid username
-            var compare = req.body.username.match(usernameRegex);
+           var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            // Var below will compare the user input with regex above to see if it is a valid email
+            var compare = req.body.email.match(emailRegex);
 
             if (!compare) {
                 /*
-                If username is invalid, a 400 error status code will be sent indicating 
-                    that the username format is invalid.
+                If email is invalid, a 400 error status code will be sent indicating 
+                    that the email format is invalid.
                 */
-                return res.status(400).send('Username is using invalid characters');
+                return res.status(400).send('Email is using invalid characters');
             } else {
                 // The user's input is then searched through the database to see if there is a match
-                const userAccount = await checkUsername(req.body.username);
+                const userAccount = await checkEmail(req.body.email);
 
                 if (userAccount !== undefined) {
                     /*
-                    If an account under than username already exists, a 400 error status code
+                    If an account under than email already exists, a 400 error status code
                         will be sent along with a message telling the user that an account under
-                        that username exists.
+                        that email exists.
                     */
-                    return res.status(400).send('That username already exists');
+                    return res.status(400).send('That email already exists');
                 } else {
                     /* 
-                    If the username is not linked to any account, the current username will be replaced
-                        with the user's new chosen username which will be updated in database.
+                    If the email is not linked to any account, the current email will be replaced
+                        with the user's new chosen email which will be updated in database.
                     */
                     try {
 
@@ -93,7 +93,7 @@ export class Api {
                         await getConnection()
                             .createQueryBuilder()
                             .update(User)
-                            .set({ username: req.body.username })
+                            .set({ email: req.body.email })
                             .where("id = :id", { id: req.body.id })
                             .execute();
 
@@ -101,7 +101,7 @@ export class Api {
                         A 201 success status code will be sent along with a message 
                             telling the user that the account was successfully created.
                         */
-                        return res.status(201).send("Username changed");
+                        return res.status(201).send("Email changed");
                     } catch (err) {
                         /* 
                         In any odd event something goes wrong whilst the account is being 
@@ -114,11 +114,11 @@ export class Api {
         });
 
         /* 
-        The checkUsername function searches through the database and checks if an account with 
-            the username exists. It will return a user if it finds a match. Otherwise, it will
+        The checkEmail function searches through the database and checks if an account with 
+            the email exists. It will return a user if it finds a match. Otherwise, it will
             return undefined.
         */
-        async function checkUsername(usernameInput: String): Promise<any> {
+        async function checkEmail(emailInput: String): Promise<any> {
             try {
                 // Establishes connection
                 const connection = await connect();
@@ -130,12 +130,12 @@ export class Api {
                 //     .from(User)
                 //     .execute();
 
-                // SELECT search query to find if a user has the same username
+                // SELECT search query to find if a user has the same email
                 const userQuery = await connection
                     .createQueryBuilder()
                     .select("user")
                     .from(User, "user")
-                    .where("user.username = :username", { username: usernameInput })
+                    .where("user.email = :email", { email: emailInput })
                     .getRawOne();
 
                 // Returns undefined if no match
