97cf96334e5c32c2e5a356eb8e6ac3a9b1c26b96
Louis Manabat <manabat.louis@gmail.com>
2020-09-21 01:31:35 +1000

    [Task] Added password changing option

--- src/api/lib/Api.ts
+++ src/api/lib/Api.ts
@@ -9,7 +9,7 @@ import { User } from "./../../orm/entities/User";
 var bodyParser = require('body-parser');
 
 /*
-The following code works on changing the account's username.
+The following code works on changing the account's password.
 
 To use and test the database:
 1. You may use Postman or use the Rest Client VSCode extenstion (ext ID: humao.rest-client).
@@ -19,9 +19,9 @@ To use and test the database:
     file to see what you need to do).
 4. If the Rest Client is installed, above the POST request link, there is a button that
     says "Send Request".
-5. To test username change, send a request under the change username request comment. You should
-    be notified that the username for that account has been changed and pressing it again shoul tell
-    that said username already exists.
+5. To test password change, send a request under the change password request comment. You should
+    be notified that the password for that account has been changed and pressing it again shoul tell
+    that said password already exists.
 6. To check, send a request under the show users request comment to see the list of users 
     and their details.
 */
@@ -39,7 +39,7 @@ export class Api {
 
     // TODO: Implement a better Route handler
     private registerRoutes(): void {
-        // A test method to see if the register/login methods work
+        // A temp method that shows all the user's details
         this.app.get('/users', async (req, res) => {
             try {
                 const connection = await connect();
@@ -52,101 +52,31 @@ export class Api {
             }
         });
 
-        // The following method is to change a user's username
-        this.app.post('/users/changeusername', async (req, res) => {
-            /*
-            The username regex variable will be compared with the username
-            the user provides. The username will be considered valid
-            based on certain conditions:
-            - If there are no illegal characters 
-            - If the beginning character is a letter
-            - Only contains lowercase letters and numbers between 0 to 9
-            - Is between 8 to 16 characters
-            */
-            var usernameRegex = /^\D[a-z0-9]{8,16}$/;
-            // Var below will compare the user input with regex above to see if it is a valid username
-            var compare = req.body.username.match(usernameRegex);
-
-            if (!compare) {
-                /*
-                If username is invalid, a 400 error status code will be sent indicating 
-                    that the username format is invalid.
-                */
-                return res.status(400).send('Username is using invalid characters');
-            } else {
-                // The user's input is then searched through the database to see if there is a match
-                const userAccount = await checkUsername(req.body.username);
-
-                if (userAccount !== undefined) {
-                    /*
-                    If an account under than username already exists, a 400 error status code
-                        will be sent along with a message telling the user that an account under
-                        that username exists.
-                    */
-                    return res.status(400).send('That username already exists');
-                } else {
-                    /* 
-                    If the username is not linked to any account, the current username will be replaced
-                        with the user's new chosen username which will be updated in database.
-                    */
-                    try {
-
-                        // Updating the local SQL database
-                        await getConnection()
-                            .createQueryBuilder()
-                            .update(User)
-                            .set({ username: req.body.username })
-                            .where("id = :id", { id: req.body.id })
-                            .execute();
-
-                        /*
-                        A 201 success status code will be sent along with a message 
-                            telling the user that the account was successfully created.
-                        */
-                        return res.status(201).send("Username changed");
-                    } catch (err) {
-                        /* 
-                        In any odd event something goes wrong whilst the account is being 
-                            created, a 500 status code will be sent.
-                        */
-                        console.log(err);
-                    }
-                }
-            }
-        });
-
-        /* 
-        The checkUsername function searches through the database and checks if an account with 
-            the username exists. It will return a user if it finds a match. Otherwise, it will
-            return undefined.
-        */
-        async function checkUsername(usernameInput: String): Promise<any> {
+        // The following method is to change a user's password
+        this.app.post('/users/changepassword', async (req, res) => {
             try {
-                // Establishes connection
-                const connection = await connect();
+                // const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
-                // Temp code
-                // await getConnection()
-                //     .createQueryBuilder()
-                //     .delete()
-                //     .from(User)
-                //     .execute();
-
-                // SELECT search query to find if a user has the same username
-                const userQuery = await connection
+                // Updating the local SQL database
+                await getConnection()
                     .createQueryBuilder()
-                    .select("user")
-                    .from(User, "user")
-                    .where("user.username = :username", { username: usernameInput })
-                    .getRawOne();
+                    .update(User)
+                    .set({ password: await bcrypt.hash(req.body.password, 10) })
+                    .where("id = :id", { id: req.body.id })
+                    .execute();
 
-                // Returns undefined if no match
-                return userQuery;
+                /*
+                A 201 success status code will be sent along with a message 
+                    telling the user that the account was successfully created.
+                */
+                return res.status(201).send("Password changed");
             } catch (err) {
+                /* 
+                In any odd event something goes wrong whilst the account is being 
+                    created, a 500 status code will be sent.
+                */
                 console.log(err);
             }
-        }
+        });
     }
-
-
 }
