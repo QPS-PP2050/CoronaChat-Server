245c4876597e2ec20f353ff7b1855d7f8e8ee1f7
CyberiumShadow <cyberiumdev@gmail.com>
2020-09-24 02:27:56 +0000

    Add placeholder files

---
+++ src/api/lib/routes/channel.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/channels/:channelId/messages', (req, res) => {
+     // Placeholder to get messages
+})
+
+router.post('/channels/:channelId/messages', (req, res) => {
+     // Placeholder to send messages
+})
+
+router.post('/channels/', (req, res) => {
+    // Placeholder to create new channel
+})
+
+router.delete('channels/:channelId', (req, res) => {
+    // Placeholder to delete channel
+})
---
+++ src/api/lib/routes/server.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.post('/server', (req, res) => {
+     // Placeholder to Create Server
+ })
+
+router.post('/server/:serverId/channels', (req, res) => {
+    // Placeholder to create new channel
+})
+
+router.delete('/server/:serverId', (req, res) => {
+    // Placeholder to delete server
+}) 
