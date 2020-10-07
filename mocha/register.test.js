3d0e32a76ebcc841d744daae0d436581a0f8ce21
Louis Manabat <manabat.louis@gmail.com>
2020-10-07 21:01:34 +1100

    [Task] register.test.js WIP

--- mocha/register.test.js
+++ mocha/register.test.js
@@ -2,16 +2,18 @@ const fetch = require('node-fetch')
 const https = require('https')
 const expect = require('expect.js')
 
-let registration = {
-    "email": "louis2@hotmail.com",
+let registrationDetails = {
+    "email": "louisM@gmail.com",
     "password": "passWord123"
 }
 
 describe('Register API', function() {
     it('Should register a new user and return 201', async function() {
+        // Add delete user here
+
         const res = await fetch('https://localhost:8080/users', {
             method: 'POST',
-            body: JSON.stringify(registration),
+            body: JSON.stringify(registrationDetailstion),
             headers: { 'Content-type': 'application/json' },
             agent: new https.Agent({ rejectUnauthorized: false})
         })
