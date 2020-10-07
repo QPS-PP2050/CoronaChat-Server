d362ea7df1807aade09947a277930b1da0592b4e
Louis Manabat <manabat.louis@gmail.com>
2020-10-07 21:02:38 +1100

    [Task] Added test case for /user/login

---
+++ mocha/login.test.js
@@ -0,0 +1,21 @@
+const fetch = require('node-fetch')
+const https = require('https')
+const expect = require('expect.js')
+
+let loginDetails = {
+    "email": "louisM@gmail.com",
+    "password": "passWord123"
+}
+
+describe('Login API', function() {
+    it('Should login an existing user and return 201', async function() {
+        const res = await fetch('https://localhost:8080/users/login', {
+            method: 'POST',
+            body: JSON.stringify(loginDetails),
+            headers: { 'Content-type': 'application/json' },
+            agent: new https.Agent({ rejectUnauthorized: false})
+        })
+        expect(res.status).to.be(201);
+        console.log(err);
+    })
+})
