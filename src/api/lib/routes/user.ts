b83451a0ddc5843b0d85e7a2cd93664c9dcc47ca
Louis Manabat <manabat.louis@gmail.com>
2020-10-07 20:28:04 +1100

    [Docs/Refactor] Lots and lots of documentation added

--- src/api/lib/routes/user.ts
+++ src/api/lib/routes/user.ts
@@ -25,15 +25,13 @@ router.get('/users', async (req, res) => {
 
 // The following post request will register a new user
 router.post('/users', async (req, res) => {
-    /* 
-    The email regex variable will be compared with the
+    /*  The email regex variable will be compared with the
         email the user provides. The email will be considered valid
         based on certain conditions:
-    - If there are no illegal characters (only dash and underscore allowed)
-    - If the beginning character is alphanumeric
-    - An '@' is present and does not have a dot before or after it
-    - No consecutive dots 
-    */
+        - If there are no illegal characters (only dash and underscore allowed)
+        - If the beginning character is alphanumeric
+        - An '@' is present and does not have a dot before or after it
+        - No consecutive dots */
     var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
     // Var below will compare the user input with regex above to see if it is a valid email
@@ -48,18 +46,14 @@ router.post('/users', async (req, res) => {
         const userAccount = await checkUserEmail(req.body.email);
 
         if (userAccount !== undefined) {
-            /* 
-            If an account under than email already exists, a 400 error status code
+            /* If an account under than email already exists, a 400 error status code
                 will be sent along with a message telling the user that an account under
-                that email exists.
-            */
+                that email exists.*/
             return res.status(400).send('Account under that email already exists');
         } else {
-            /*
-            If the email is not linked to any account, the password will be hashed
+            /* If the email is not linked to any account, the password will be hashed
                 using salt and the email and hashed password will be pushed to the users
-                database.
-            */
+                database. */
             try {
                 // Hashes the password using bycrpt
                 const hashedPassword = await bcrypt.hash(req.body.password, 10);
@@ -75,22 +69,19 @@ router.post('/users', async (req, res) => {
                 } catch (err) {
                     console.log(err);
                 }
-                /* 
-                A 201 success status code will be sent along with a message 
-                    telling the user that the account was successfully created.
-                */
+                /* A 201 success status code will be sent along with a message 
+                    telling the user that the account was successfully created. */
                 res.status(201).send('Account created');
             } catch {
-                /* 
-                In any odd event something goes wrong whilst the account is being 
-                    created, a 500 status code will be sent.
-                */
+                /* In any odd event something goes wrong whilst the account is being 
+                    created, a 500 status code will be sent. */
                 res.status(500).send('Unknown Error');
             }
         }
     }
 })
 
+// This following post request will login an existing user from the database
 router.post('/users/login', async (req, res) => {
     // The user's input is then searched through the database to see if there is a match
     const userAccount = await checkUserEmail(req.body.email);
@@ -98,41 +89,34 @@ router.post('/users/login', async (req, res) => {
     /* If the account already exists, a 400 status code error will be sent along with 
         a message telling the user there is no account under that email. */
     if (userAccount == undefined) {
-
         return res.status(400).send('Account under this email/username does not exist');
     } else {
-        /* 
-        If the email exists in the database, the database hashed password
+        /* If the email exists in the database, the database hashed password
             will be compared with the password the user inputted. If the passwords match, a
-            response will be sent telling the user that they have successfully logged in.
-        */
+            response will be sent telling the user that they have successfully logged in. */
         try {
             if (!await bcrypt.compare(req.body.password, userAccount.user_password)) {
                 res.send('Login failed');
             } else {
                 res.send('Success');
-
             }
         } catch {
-            /* 
-            In any odd event something goes wrong whilst the user is trying to
-                log in, a 500 status code will be sent.
-            */
+            /* In any odd event something goes wrong whilst the user is trying to
+                log in, a 500 status code will be sent. */
             res.status(500).send();
         }
     }
 });
 
+// This post request will change a user's username (assuming it doesn't already exist)
 router.post('/users/changeusername', async (req, res) => {
-    /*
-        The username regex variable will be compared with the username
+    /*  The username regex variable will be compared with the username
         the user provides. The username will be considered valid
         based on certain conditions:
         - If there are no illegal characters 
         - If the beginning character is a letter
         - Only contains lowercase letters and numbers between 0 to 9
-        - Is between 8 to 16 characters
-        */
+        - Is between 8 to 16 characters */
     var usernameRegex = /^\D[a-z0-9]{8,16}$/;
     // Var below will compare the user input with regex above to see if it is a valid username
     var compare = req.body.username.match(usernameRegex);
@@ -143,13 +127,11 @@ router.post('/users/changeusername', async (req, res) => {
     /* If the account does not exists, a 400 status code error will be sent along with 
         a message telling the user there is no account under that ID. */
     if (userAccount == undefined) {
-        return res.status(500).send('Account with that User ID does not exist');
+        return res.status(400).send('Account with that User ID does not exist');
     } else {
         if (!compare) {
-            /*
-            If username is invalid, a 400 error status code will be sent indicating 
-                that the username format is invalid.
-            */
+            /* If username is invalid, a 400 error status code will be sent indicating 
+                that the username format is invalid. */
             return res.status(400).send('Username is using invalid characters');
         } else {
             // The user's input is then searched through the database to see if there is a match
@@ -164,10 +146,10 @@ router.post('/users/changeusername', async (req, res) => {
                 /* If the username is not linked to any account, the current username will be replaced
                     with the user's new chosen username which will be updated in database. */
                 try {
-                    // Initialising connection
+                    // Establishes connection
                     const connection = await connect();
 
-                    // Updating the local SQL database
+                    // Updating the SQL database
                     await connection
                         .createQueryBuilder()
                         .update(User)
@@ -181,23 +163,28 @@ router.post('/users/changeusername', async (req, res) => {
                 } catch (err) {
                     /* In any odd event something goes wrong whilst the account is being 
                         created, a 500 status code will be sent. */
-                    console.log(err);
+                    res.status(500).send();
                 }
             }
         }
     }
 })
 
+// This post request will change a user's password 
 router.post('/users/changepassword', async (req, res) => {
+    // Checks if a user with a certain ID exists
     const userAccount = await accountcheck(req.body.id);
 
-
+    /* If the account does not exists, a 400 status code error will be sent along with 
+        a message telling the user there is no account under that ID. */
     if (userAccount == undefined) {
-        return res.status(500).send('Account with that User ID does not exist');
+        return res.status(400).send('Account with that User ID does not exist');
     } else {
         try {
-            // Updating the local SQL database
+            // Establishes connection
             const connection = await connect();
+
+            // Updating the SQL database
             await connection
                 .createQueryBuilder()
                 .update(User)
@@ -205,65 +192,60 @@ router.post('/users/changepassword', async (req, res) => {
                 .where("id = :id", { id: req.body.id })
                 .execute();
 
-            /*
-            A 201 success status code will be sent along with a message 
-                telling the user that the account was successfully created.
-            */
+            /* A 201 success status code will be sent along with a message 
+                telling the user that the account was successfully created. */
             return res.status(201).send("Password changed");
         } catch (err) {
-            /* 
-            In any odd event something goes wrong whilst the account is being 
-                created, a 500 status code will be sent.
-            */
-            console.log(err);
+            /* In any odd event something goes wrong whilst the account is being 
+                created, a 500 status code will be sent. */
+            res.status(500).send();
         }
     }
 })
 
+// This post request will change a user's email (assuming it doesn't already exist)
 router.post('/user/changeemail', async (req, res) => {
-    /* 
-           The email regex variable will be compared with the
-               email the user provides. The email will be considered valid
-               based on certain conditions:
-           - If there are no illegal characters (only dash and underscore allowed)
-           - If the beginning character is alphanumeric
-           - An '@' is present and does not have a dot before or after it
-           - No consecutive dots 
-           */
+    /* The email regex variable will be compared with the
+        email the user provides. The email will be considered valid
+        based on certain conditions:
+        - If there are no illegal characters (only dash and underscore allowed)
+        - If the beginning character is alphanumeric
+        - An '@' is present and does not have a dot before or after it
+        - No consecutive dots */
     var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
     // Var below will compare the user input with regex above to see if it is a valid email
     var compare = req.body.email.match(emailRegex);
+
+    // Checks if a user with a certain ID exists
     const userAccount = await accountcheck(req.body.id);
 
+    /* If the account does not exists, a 400 status code error will be sent along with 
+        a message telling the user there is no account under that ID. */
     if (userAccount == undefined) {
-        return res.status(500).send('Account with that User ID does not exist');
+        return res.status(400).send('Account with that User ID does not exist');
     } else {
+        /* If email is invalid, a 400 error status code will be sent indicating 
+            that the email format is invalid. */
         if (!compare) {
-            /*
-            If email is invalid, a 400 error status code will be sent indicating 
-                that the email format is invalid.
-            */
             return res.status(400).send('Email is using invalid characters');
         } else {
             // The user's input is then searched through the database to see if there is a match
             const userAccount = await checkUserEmail(req.body.email);
 
             if (userAccount !== undefined) {
-                /*
-                If an account under than email already exists, a 400 error status code
+                /* If an account under than email already exists, a 400 error status code
                     will be sent along with a message telling the user that an account under
-                    that email exists.
-                */
+                    that email exists. */
                 return res.status(400).send('That email already exists');
             } else {
-                /* 
-                If the email is not linked to any account, the current email will be replaced
-                    with the user's new chosen email which will be updated in database.
-                */
+                /* If the email is not linked to any account, the current email will be replaced
+                    with the user's new chosen email which will be updated in database. */
                 try {
-
+                    // Establishes connection
                     const connection = await connect();
-                    // Updating the local SQL database
+
+                    // Updating the SQL database
                     await connection
                         .createQueryBuilder()
                         .update(User)
@@ -271,32 +253,34 @@ router.post('/user/changeemail', async (req, res) => {
                         .where("id = :id", { id: req.body.id })
                         .execute();
 
-                    /*
-                    A 201 success status code will be sent along with a message 
-                        telling the user that the account was successfully created.
-                    */
+                    /*A 201 success status code will be sent along with a message 
+                        telling the user that the account was successfully created. */
                     return res.status(201).send("Email changed");
                 } catch (err) {
-                    /* 
-                    In any odd event something goes wrong whilst the account is being 
-                        created, a 500 status code will be sent.
-                    */
-                    console.log(err);
+                    /* In any odd event something goes wrong whilst the account is being 
+                        created, a 500 status code will be sent. */
+                    res.status(500).send();
                 }
             }
         }
     }
 })
 
-
+// This delete request will complete delete and remove the user from the database
 router.delete('/users/:userID', async (req, res) => {
+    // Checks if a user with a certain ID exists
     const userAccount = await accountcheck(req.body.id);
 
+    /* If the account does not exists, a 400 status code error will be sent along with 
+        a message telling the user there is no account under that ID. */
     if (userAccount == undefined) {
-        return res.status(500).send('Account with that User ID does not exist');
+        return res.status(400).send('Account with that User ID does not exist');
     } else {
         try {
+            // Establishes connection
             const connection = await connect();
+
+            // Updating the SQL database
             await connection
                 .createQueryBuilder()
                 .delete()
@@ -304,7 +288,9 @@ router.delete('/users/:userID', async (req, res) => {
                 .where("user.id = :id", { id: req.params.userID })
                 .execute();
         } catch (err) {
-            console.log(err);
+            /* In any odd event something goes wrong whilst the account is being 
+                created, a 500 status code will be sent. */
+            res.status(500).send();
         }
     }
 })
