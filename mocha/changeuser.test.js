c23438824a60e648c858ac293625d59c186d6f0f
Louis Manabat <manabat.louis@gmail.com>
2020-10-09 00:11:43 +1100

    [Fix] Fixed the paths of the test cases
    Test cases should now work but still need to add more to it

--- mocha/changeuser.test.js
+++ mocha/changeuser.test.js
@@ -12,13 +12,13 @@ let changeUserDetails = {
 
 describe('Login API', function() {
     it('Should change an existing users username and return 201', async function() {
-        const res = await fetch('https://localhost:8080/users/changeusername', {
+        const res = await fetch('https://localhost:8080/api/users/changeusername', {
             method: 'POST',
             body: JSON.stringify(changeUserDetails),
             headers: { 'Content-type': 'application/json' },
             agent: new https.Agent({ rejectUnauthorized: false})
         })
         expect(res.status).to.be(201);
-        console.log(err);
+        done();
     })
 })
--- mocha/login.test.js
+++ mocha/login.test.js
@@ -9,13 +9,13 @@ let loginDetails = {
 
 describe('Login API', function() {
     it('Should login an existing user and return 201', async function() {
-        const res = await fetch('https://localhost:8080/users/login', {
+        const res = await fetch('https://localhost:8080/api/users/login', {
             method: 'POST',
             body: JSON.stringify(loginDetails),
             headers: { 'Content-type': 'application/json' },
             agent: new https.Agent({ rejectUnauthorized: false})
         })
         expect(res.status).to.be(201);
-        console.log(err);
+        done();
     })
 })
