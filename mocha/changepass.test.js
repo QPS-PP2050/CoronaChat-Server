9f5733081cf86b19c1868ceed178d73f4a85fc4c
Louis Manabat <manabat.louis@gmail.com>
2020-10-14 22:47:20 +1100

    [Fix] Amended path for test case

--- mocha/changepass.test.js
+++ mocha/changepass.test.js
@@ -3,19 +3,18 @@ const https = require('https')
 const expect = require('expect.js')
 
 let changeUserDetails = {
-    "id": 13193204077821952,
     "password": "louismanabat123"
 }
 
-describe('Login API', function() {
+describe('Change Password API', function() {
     it('Should change an existing users password and return 201', async function() {
-        const res = await fetch('https://localhost:8080/api/users/changepassword', {
-            method: 'POST',
+        // The ID in path will vary due to the snowflake algorithm
+        const res = await fetch('https://localhost:8080/api/users/15754302186455040', {
+            method: 'PATCH',
             body: JSON.stringify(changeUserDetails),
             headers: { 'Content-type': 'application/json' },
             agent: new https.Agent({ rejectUnauthorized: false})
         })
         expect(res.status).to.be(201);
-        done();
     })
 })
