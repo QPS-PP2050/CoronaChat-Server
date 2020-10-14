78c423ff49241bd449f79919a42e0bce0ece5e36
Louis Manabat <manabat.louis@gmail.com>
2020-10-14 22:47:12 +1100

    [Fix] Amended path for test case

--- mocha/changeuser.test.js
+++ mocha/changeuser.test.js
@@ -6,19 +6,18 @@ var date = new Date();
 var seconds = date.getTime() / 10000000000;
 
 let changeUserDetails = {
-    "id": 13193204077821952,
     "username": "louismanabat" + Math.round(seconds)
 }
 
-describe('Login API', function() {
+describe('Change Username API', function() {
     it('Should change an existing users username and return 201', async function() {
-        const res = await fetch('https://localhost:8080/api/users/changeusername', {
-            method: 'POST',
+        // The ID in path will vary due to the snowflake algorithm
+        const res = await fetch('https://localhost:8080/api/users/15754302186455040', {
+            method: 'PATCH',
             body: JSON.stringify(changeUserDetails),
             headers: { 'Content-type': 'application/json' },
             agent: new https.Agent({ rejectUnauthorized: false})
         })
         expect(res.status).to.be(201);
-        done();
     })
 })
