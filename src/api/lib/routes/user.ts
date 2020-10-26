17df9a749a2adcb4ee94379f4f6ae33bc268071b
Louis Manabat <manabat.louis@gmail.com>
2020-10-27 02:01:19 +1100

    [Task] Removed change email feature completely

--- src/api/lib/routes/user.ts
+++ src/api/lib/routes/user.ts
@@ -143,57 +143,6 @@ router.patch('/users/:userID', async (req, res) => {
                 }
             }
         }
-    } else if (req.body.email !== undefined) {
-        /* The email regex variable will be compared with the
-        email the user provides. The email will be considered valid
-        based on certain conditions:
-        - If there are no illegal characters (only dash and underscore allowed)
-        - If the beginning character is alphanumeric
-        - An '@' is present and does not have a dot before or after it
-        - No consecutive dots */
-        var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
-        // Var below will compare the user input with regex above to see if it is a valid email
-        var compare = req.body.email.match(emailRegex);
-
-        /* If email is invalid, a 400 error status code will be sent indicating 
-            that the email format is invalid. */
-        if (!compare) {
-            return res.status(400).send({ reason: 'Email is using invalid characters' });
-        } else {
-            // The user's input is then searched through the database to see if there is a match
-            const userAccount = await checkUserEmail(req.body.email);
-
-            if (userAccount !== undefined) {
-                /* If an account under than email already exists, a 400 error status code
-                    will be sent along with a message telling the user that an account under
-                    that email exists. */
-                return res.status(400).send({ reason: 'That email already exists' });
-            } else {
-                /* If the email is not linked to any account, the current email will be replaced
-                    with the user's new chosen email which will be updated in database. */
-                try {
-                    // Establishes connection
-                    const connection = await connect();
-
-                    // Updating the SQL database
-                    await connection
-                        .createQueryBuilder()
-                        .update(User)
-                        .set({ email: req.body.email })
-                        .where("id = :id", { id: req.params.userID })
-                        .execute();
-
-                    /*A 201 success status code will be sent along with a message 
-                        telling the user that the account was successfully created. */
-                    return res.status(201).send({ reason: 'Email changed' });
-                } catch (err) {
-                    /* In any odd event something goes wrong whilst the account is being 
-                        created, a 500 status code will be sent. */
-                    res.status(500).send({ reason: 'Unknown Error' });
-                }
-            }
-        }
     } else if (req.body.password !== undefined) {
         try {
             // Establishes connection
